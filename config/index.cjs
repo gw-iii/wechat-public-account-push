3b311dd2c79352908ea0948e21fec8ade74619e9
gw-iii <125015012+gw-iii@users.noreply.github.com>
2023-02-17 10:24:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onfsI6LzSrOeY_H24rpclFnSn4Jo',
+      id: 'onfsI6LuvNwbkGua-gFmu-JtrqVY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'sOM-Xswa_yeXY7U8HrvE1wciQNfPvKjjqzeyqycQOdA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -37,6 +37,32 @@ const USER_CONFIG = {
         },
       ],
       // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-12-04' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      name: '小朋友2',
+      id: 'onfsI6LuvNwbkGua-gFmu-JtrqVY',
+      useTemplateId: 'sOM-Xswa_yeXY7U8HrvE1wciQNfPvKjjqzeyqycQOdA',
+      province: '湖南',
+      city: '长沙',
+      horoscopeDate: '04-29',
+      horoscopeDateType: '',
+      openUrl: 'https://wangxinleo.cn',
+      festivals:  [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小朋友', year: '1997', date: '04-29',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '12-04',
+        },
+      ],
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-12-04' },
